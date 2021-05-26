03446ae946315b5fbc477cc7667a4502322c7113
Rian Iregho <rianiregho@gmail.com>
2021-05-26 13:26:17 +0700

    Adding More Axios Service which need Auth Token

--- src/config/Axios/index.js
+++ src/config/Axios/index.js
@@ -1,5 +1,11 @@
 import axios from "axios"
 import BASE_URL from "../BaseUrl"
+import { AsyncStorage } from 'react-native';
+
+const access_token = async () => {
+    let token = await AsyncStorage.getItem('access_token')
+    return token
+}
 
 const POST = async (endpoint,data) => {
     let result = false
@@ -14,6 +20,33 @@ const POST = async (endpoint,data) => {
 }
 
 
+const POSTAUTH = async (endpoint,data) => {
+    let result = false
+    await axios.post(`${BASE_URL}${endpoint}`,data, { headers: {"Authorization" : `Bearer ${access_token()}`} })
+    .then(response => {
+        result = response
+    })
+    .catch((error) => {
+        result = error.response.status
+    })
+    return result
+}
+
+const GETAUTH = async (endpoint) => {
+    let result = false
+    await axios.post(`${BASE_URL}${endpoint}`, { headers: {"Authorization" : `Bearer ${access_token()}`} })
+    .then(response => {
+        result = response
+    })
+    .catch((error) => {
+        result = error.response.status
+    })
+    return result
+}
+
+
 export {
-    POST
+    POST,
+    POSTAUTH,
+    GETAUTH
 }
