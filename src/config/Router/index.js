98e6f98654b87366a7381ac62f97c8aae4759aec
Rian Iregho <rianiregho@gmail.com>
2021-06-02 14:04:33 +0700

    Fix redux Bookreducer punya Tubagus

--- src/config/Router/index.js
+++ src/config/Router/index.js
@@ -16,10 +16,10 @@ const Router = () => {
                 {/* <Stack.Screen name="SplashScreen" component={SplashScreen} /> */}
                 {/* <Stack.Screen name="LoginPage" component={LoginPage} /> */}
                 {/* <Stack.Screen name="SplashScreen" component={SplashScreen} /> */}
-                <Stack.Screen name="LoginPage" component={LoginPage} />
+                {/* <Stack.Screen name="LoginPage" component={LoginPage} /> */}
                 {/* <Stack.Screen name="ProfilePage" component={ProfilePage} /> */}
                 <Stack.Screen name="BooksPage" component={BooksPage} />
-                <Stack.Screen name="HomePage" component={HomePage} />
+                {/* <Stack.Screen name="HomePage" component={HomePage} /> */}
 
             </Stack.Navigator>
         </>
--- src/containers/organisms/Books/index.js
+++ src/containers/organisms/Books/index.js
@@ -35,7 +35,7 @@ const Books = (props) => {
         let result = send.data.data
         let array = books ?? []
         result.data.map(item =>{
-            array.push(item)
+            array.push(item) 
         })
 
         let dataBookState = {
@@ -49,12 +49,13 @@ const Books = (props) => {
         // })
     }
     useEffect(() => {
+        console.log(props.booksData)
         // if(props.booksData.page == 1){
         //     // getDataBooks()
             
         // }
-        getDataBooks()
-        console.log(dataBookState)
+        // getDataBooks()
+        // console.log(dataBookState)
     },[])
     return (
         <View style={styles.container}>
@@ -170,4 +171,4 @@ const mapDispatchToProps = (dispatch) => {
         updateBook: (value) => {dispatch(SET_BOOK_DATA(value))},
     }
 }
-export default connect(mapDispatchToProps,mapStateToProps)(Books);
+export default connect(mapStateToProps,mapDispatchToProps)(Books);
