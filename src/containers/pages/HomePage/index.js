d489e2b12b7b85694fb03f69f034888ac28b10b4
Riko <rikowie860@gmail.com>
2021-07-04 15:26:35 +0700

    sada

--- src/containers/pages/HomePage/index.js
+++ src/containers/pages/HomePage/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { TouchableOpacity } from 'react-native';
 import { Button, Dimensions, ScrollView, View, Text } from 'react-native'
 import { SliderBox } from "react-native-image-slider-box"
+import LinearGradient from 'react-native-linear-gradient';
 import NavigationReport from '../../../components/moleculs/NavigationReport'
 import { GET } from '../../../config/Axios';
 
@@ -60,17 +61,73 @@ const HomePage = ({navigation, style, ...props}) => {
                     :
                     <Text>Loading ...</Text>
                 }
-            <Text>Hi</Text>
             <TouchableOpacity style={{backgroundColor:"red"}}>
-                </TouchableOpacity>    
-            </ScrollView>  
-
+                </TouchableOpacity>  
+                <Text style={{textAlign:'center', fontSize:20, fontWeight:'bold'}}>Siswa Terpopuler</Text>
+                <Text style={{textAlign:'center'}}>Siswa Terpopuler berdasarkan jumlah point terbanyak akumulasi peminjaman</Text>
+                <View style={{overflow:'hidden', flexDirection:'row', flexWrap:'wrap', justifyContent:'space-between'}}>
+                    <View style={{height:120, width:120 ,backgroundColor:"#2bdbdc", margin:10, borderRadius:10}}>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <TouchableOpacity style={{borderWidth:1, borderRadius:10, borderStyle:'solid',backgroundColor:'white', marginTop:5, marginLeft:30, marginRight:30}}><Text style={{textAlign:'center'}}>Kilk</Text></TouchableOpacity>
+                    </View>
+                    <View style={{height:120, width:120, backgroundColor:"#2bdbdc", margin:10, borderRadius:10}}>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <TouchableOpacity style={{borderWidth:1, borderRadius:10, borderStyle:'solid',backgroundColor:'white', marginTop:5, marginLeft:30, marginRight:30}}><Text style={{textAlign:'center'}}>Kilk</Text></TouchableOpacity>
+                    </View>
+                    <View style={{height:120, width:120 ,backgroundColor:"#2bdbdc", margin:10, borderRadius:10}}>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <TouchableOpacity style={{borderWidth:1, borderRadius:10, borderStyle:'solid',backgroundColor:'white', marginTop:5, marginLeft:30, marginRight:30}}><Text style={{textAlign:'center'}}>Kilk</Text></TouchableOpacity>
+                    </View>
+                    <View style={{height:120, width:120, backgroundColor:"#2bdbdc", margin:10, borderRadius:10}}>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <TouchableOpacity style={{borderWidth:1, borderRadius:10, borderStyle:'solid',backgroundColor:'white', marginTop:5, marginLeft:30, marginRight:30}}><Text style={{textAlign:'center'}}>Kilk</Text></TouchableOpacity>
+                    </View>
+                    <View style={{height:120, width:120, backgroundColor:"#2bdbdc", margin:10, borderRadius:10}}>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <TouchableOpacity style={{borderWidth:1, borderRadius:10, borderStyle:'solid',backgroundColor:'white', marginTop:5, marginLeft:30, marginRight:30}}><Text style={{textAlign:'center'}}>Kilk</Text></TouchableOpacity>
+                    </View>
+                    <View style={{height:120, width:120, backgroundColor:"#2bdbdc", margin:10, borderRadius:10}}>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <TouchableOpacity style={{borderWidth:1, borderRadius:10, borderStyle:'solid',backgroundColor:'white', marginTop:5, marginLeft:30, marginRight:30}}><Text style={{textAlign:'center'}}>Kilk</Text></TouchableOpacity>
+                    </View>
+                    <View style={{height:120, width:120, backgroundColor:"#2bdbdc", margin:10, borderRadius:10}}>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <TouchableOpacity style={{borderWidth:1, borderRadius:10, borderStyle:'solid',backgroundColor:'white', marginTop:5, marginLeft:30, marginRight:30}}><Text style={{textAlign:'center'}}>Kilk</Text></TouchableOpacity>
+                    </View>
+                    <View style={{height:120, width:120, backgroundColor:"#2bdbdc", margin:10, borderRadius:10}}>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <TouchableOpacity style={{borderWidth:1, borderRadius:10, borderStyle:'solid',backgroundColor:'white', marginTop:5, marginLeft:30, marginRight:30}}><Text style={{textAlign:'center'}}>Kilk</Text></TouchableOpacity>
+                    </View>
+                    <View style={{height:120, width:120, backgroundColor:"#2bdbdc", margin:10, borderRadius:10}}>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <Text style={{textAlign:'center', color:'white', marginTop:10}}>Hi</Text>
+                        <TouchableOpacity style={{borderWidth:1, borderRadius:10, borderStyle:'solid',backgroundColor:'white', marginTop:5, marginLeft:30, marginRight:30}}><Text style={{textAlign:'center'}}>Kilk</Text></TouchableOpacity>
+                    </View>
+                </View>
+            </ScrollView> 
             <TouchableOpacity onPress={() => {check()}}>
                 <Text>Click dong</Text> 
             </TouchableOpacity> 
 
         </View>
-        <View style={{height:100, padding:20, marginTop:-10}}>
+        <View style={{height:100, marginTop:100, marginTop:-10}}>
             <NavigationReport/>
         </View>
         </>
